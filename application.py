358f31aa5662fea681e87ec5d7ba2ba6da1e5f36
s362106 <s362106@oslomet.no>
2023-04-22 17:13:13 +0200

    Startet på DRTPHeader

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ import struct
 
 
 # Define the simplified TCP header structure
-class TCPSimpleHeader:
+class DRTPHeader:
     def __init__(self, seq_num, ack_num, syn_flag, ack_flag, fin_flag, reset_flag, window_size):
 
         self.seq_num = seq_num
@@ -20,18 +20,21 @@ class TCPSimpleHeader:
         self.reset_flag = reset_flag
         self.window_size = window_size
 
+    # This function packs inputed values into a header
     def pack(self):
         # Pack the TCP header fields into a binary string
         flags = (self.syn_flag << 3) | (self.ack_flag << 2) | (self.fin_flag << 1) | (self.reset_flag)
 
-        tcp_header = struct.pack('!LLHH',
+        # L = 4 bytes, H = 2 bytes, ! = big-endian
+        drtp_header = struct.pack('!LLHH',
 
                                  self.seq_num,
                                  self.ack_num,
                                  flags,
                                  self.window_size)
-        return tcp_header
+        return drtp_header
 
+    # This function unpacks inputed values
     @classmethod
     def unpack(cls, packed_data):
         # Unpack a binary string into TCP header fields
