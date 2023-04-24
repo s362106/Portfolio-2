b1bdf408319f207326223d540bf6318292fa9f21
s362106 <s362106@oslomet.no>
2023-04-24 20:23:45 +0200

    Code can send data from client to server now

--- application.py
+++ application.py
@@ -6,52 +6,198 @@ import sys
 import threading
 import re
 import struct
+from drtp import *
+
+BUFFER_SIZE = 1472
 
 
 # Define the simplified TCP header structure
-class DRTPHeader:
-    def __init__(self, seq_num, ack_num, syn_flag, ack_flag, fin_flag, reset_flag, window_size):
 
-        self.seq_num = seq_num
-        self.ack_num = ack_num
-        self.syn_flag = syn_flag
-        self.ack_flag = ack_flag
-        self.fin_flag = fin_flag
-        self.reset_flag = reset_flag
-        self.window_size = window_size
-
-    # This function packs inputed values into a header
-    def pack(self):
-        # Pack the TCP header fields into a binary string
-        flags = (self.syn_flag << 3) | (self.ack_flag << 2) | (self.fin_flag << 1) | (self.reset_flag)
-
-        # L = 4 bytes, H = 2 bytes, ! = big-endian
-        drtp_header = struct.pack('!LLHH',
-
-                                 self.seq_num,
-                                 self.ack_num,
-                                 flags,
-                                 self.window_size)
-        return drtp_header
-
-    # This function unpacks inputed values
-    @classmethod
-    def unpack(cls, packed_data):
-        # Unpack a binary string into TCP header fields
-        seq_num, ack_num, flags, window_size= struct.unpack('!LLHH', packed_data)
-        syn_flag = (flags >> 3) & 1
-        ack_flag = (flags >> 2) & 1
-        fin_flag = (flags >> 1) & 1
-        reset_flag = flags & 1
-        return cls(seq_num, ack_num, syn_flag, ack_flag, fin_flag, reset_flag, window_size)
+file_path = 'received_file.png'
 
 
-def checkIP(address):
+def run_server(ip, port):
     try:
-        ip = ipaddress.IPv4Address(address)
+        server_socket = socket(AF_INET, SOCK_DGRAM)
+        server_socket.bind((ip, port))
 
-    except:
-        raise
+        print(f"Server listening on {ip}:{port}")
+
+    except Exception as e:
+        print("Failed to bind. Error:", e)
+        sys.exit()
+
+    print("Receiving data")
+    with open(file_path, 'wb') as file:
+        while True:
+            data, addr = server_socket.recvfrom(BUFFER_SIZE)
+            header_msg = data[:12]
+            msg = data[12:]
+            #print(f"Header length:", len(header_msg))
+
+            seq, ack_nr, flags, win = parse_header(header_msg)
+            print("Window size is:", win)
+
+            syn, ack, fin = parse_flags(flags)
+            print(f"Header values seq={seq}, ack={ack}, and fin={fin}")
+
+            file.write(msg)
+
+            if fin == 1:
+                ack_nr = seq +1
+                flags = 6
+                data = b''
+                packet = create_packet(seq, ack_nr, flags, 0, data)
+
+                server_socket.sendto(packet, addr)
+                break
+
+
+            # check for SYN message
+            '''if syn == 1:
+                print("Received SYN message")
+                data = b''
+                sequence_nr = 0
+                acknowledgement_nr = seq + 1
+                window = 0
+                flags = 12
+    
+                # Send SYN-ACK message
+                SYN_ACK = create_packet(sequence_nr, acknowledgement_nr, flags, window, data)
+    
+                server_socket.sendto(SYN_ACK, addr)
+                print("Sent SYN-ACK message")
+    
+            if ack == 1 and syn != 1:
+                print("Received final ACK message")
+                break'''
+
+def run_client(server_ip, server_prt):
+    try:
+        client_sock = socket(AF_INET, SOCK_DGRAM)
+
+    except Exception as e:
+        print("Failed to send data. Error:", e)
+        sys.exit()
+
+    file_path = '/Users/fahmimohammed/Screenshot 2023-04-24 at 20.15.05.png'
+
+    print("Åpner filen")
+    file = open(file_path, 'rb')
+
+    sequence_number = 0
+    while True:
+        data = file.read(1040)
+
+        if not data:
+            print("Sender FIN message")
+            flags = 2
+            data = b''
+            packet = create_packet(sequence_number, 0, flags, 64, data)
+
+            client_sock.sendto(packet, (server_ip, server_prt))
+            break
+
+        packet = create_packet(seq=sequence_number, ack=0, flags=0, win=0, data=data)
+        client_sock.sendto(packet, (server_ip, server_prt))
+        sequence_number += 1
+    client_sock.close()
+    '''sequence_nr = 0
+    acknowledgement_nr = 0
+    window = 64
+    flags = 8
+    data = b''
+    SYN = create_packet(sequence_nr, acknowledgement_nr, flags, window, data)
+
+    client_sock.sendto(SYN, (server_ip, server_prt))
+    print("Sent SYN message")
+
+    while True:
+        print("Receiving data")
+        server_packet, addr = client_sock.recvfrom(BUFFER_SIZE)
+        header = server_packet[:12]
+
+        seq, ack, flags, win = parse_header(header)
+        print("Window size is:", win)
+
+        syn, ack, fin = parse_flags(flags)
+        print(f"Header values seq={syn}, ack={ack}, and fin={fin}")
+
+        # ACK
+        if syn == 1 and ack == 1:
+            print("Received SYN-ACK message")
+            data = b''
+            sequence_nr = 1
+            acknowledgement_nr = seq + 1
+            window = 64
+            flags = 4
+            ACK = create_packet(sequence_nr, acknowledgement_nr, flags, window, data)
+
+            client_sock.sendto(ACK, addr)
+            print("Sent ACK message")'''
+
+
+
+'''def run_server(ip, port):
+    try:
+        server_socket = socket(AF_INET, SOCK_DGRAM)
+        server_socket.bind((ip, port))
+
+        print(f"Server listening on {ip}:{port}")
+
+    except Exception as e:
+        print("Failed to bind. Error:", e)
+        sys.exit()
+
+    file_path = 'received_file.txt'
+
+    with open(file_path, 'wb') as file:
+        while True:
+            data, addr = server_socket.recvfrom(BUFFER_SIZE)
+
+            if not data:
+                break
+
+            file.write(data)
+
+
+    server_socket.close()
+
+def run_client(server_ip, server_prt):
+        try:
+            client_sock = socket(AF_INET, SOCK_DGRAM)
+
+        except Exception as e:
+            print("Failed to send data. Error:", e)
+            sys.exit()
+
+        file_path = '/Users/fahmimohammed/fil.txt'
+
+        file = open(file_path, 'rb')
+        while True:
+
+            file_data = file.read(1460)
+
+            if not file_data:
+                break
+
+            sequence_nr = 0
+            acknowledgement_nr = 0
+            window = 64
+            flags = 0
+            header = create_packet(sequence_nr, acknowledgement_nr, flags, window, file_data)
+
+            client_sock.sendto(header, (server_ip, server_prt))
+
+        file.close()
+        client_sock.close()'''
+
+def check_ip(address):
+    try:
+        ipaddress.ip_address(address)
+
+    except ValueError:
+        print(f"The IP address {address} is not valid")
 
 
 if __name__ == '__main__':
@@ -59,9 +205,18 @@ if __name__ == '__main__':
 
     parser.add_argument('-s', '--server', action='store_true', help='Run in server mode')
     parser.add_argument('-p', '--port', type=int, help='Port number to listen on')
+    parser.add_argument('-f', '--file_name', type=str, help='File name to store the data in')
+    parser.add_argument('-r', '--reliability', type=str, help='Choose reliability of the data transfer')
     parser.add_argument('-b', '--bind', type=str)
 
     parser.add_argument('-c', '--client', action='store_true', help='Run in client mode')
 
+    args = parser.parse_args()
 
+    check_ip(args.bind)
+    if args.server:
+        run_server(args.bind, args.port)
+
+    elif args.client:
+        run_client(args.bind, args.port)
 
--- drtp.py
+++ drtp.py
@@ -1,8 +1,74 @@
-from socket import *
+from struct import  *
+
+header_format = '!IIHH'
+def create_packet(seq, ack, flags, win, data):
+    header = pack(header_format, seq, ack, flags, win)
+
+    packet = header + data
+
+    #print(f'Packet containing header + data of size {len(packet)}')
+
+    return packet
+
+
+def parse_header(header):
+    header_from_msg = unpack(header_format, header)
+    return header_from_msg
+
+
+def parse_flags(flags):
+    syn = (flags >> 3) & 1
+    ack = (flags >> 2) & 1
+    fin = (flags >> 1) & 1
+
+    return syn, ack, fin
+
+
+
+'''sequence_nr = 1
+acknowledgement_nr = 0
+window = 0
+flags = 0
+
+data = bytes(1460)
+
+msg = create_packet(sequence_nr, acknowledgement_nr, window, flags, data)
+
+header_from_msg = msg[:12]
+print('Header form message length:',len(header_from_msg))
+
+seq, ack, flags, win = parse_header(header_from_msg)
+
+data_from_msg = msg[12:]
+print('Data length:',len(data_from_msg))
+
+
+data = b''
+print('\n\nCreating an aknowledgment pakcet:')
+print(f'This is an empty packet with no data = {len(data)}')
+
+sequence_nr = 0
+acknowledgement_nr = 1
+window = 0
+
+flags = 4
+
+msg = create_packet(sequence_nr, acknowledgement_nr, flags, window, data)
+print(f'This is an aknowledgement packet of size {len(msg)}')
+
+seq, ack, flags, win = parse_header(msg)
+print(f'seq={seq}, ack={ack}, flags={flags}, receiver-window={win}')
+
+syn, ack, fin = parse_flags(flags)
+print (f'syn_flag = {syn}, ack_flag={ack}, and fin_flag={fin}')'''
+
+
+
+'''from socket import *
 import sys
 import struct
 
-class DRTPHeader:
+class DRTP:
     def __init__(self, seq_num, ack_num, syn_flag, ack_flag, fin_flag, reset_flag, window_size):
 
         self.seq_num = seq_num
@@ -40,3 +106,4 @@ class DRTPHeader:
         except struct.error as e:
             print("Error unpacking DRTPHeader:", e)
             return None
+'''
