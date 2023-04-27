71f6508ba2aff182c0e2eb5308aab0c8816acd53
s362106 <s362106@oslomet.no>
2023-04-27 21:42:40 +0200

    The application can send a big file now using the custom protocol DRTP

--- application.py
+++ application.py
@@ -10,8 +10,6 @@ HEADER_SIZE = 12
 
 # Define the simplified TCP header structure
 
-drtp = DRTP()
-
 def check_ip(address):
     try:
         ipaddress.ip_address(address)
@@ -25,40 +23,45 @@ def run_server(ip, port):
     try:
         server_socket = socket(AF_INET, SOCK_DGRAM)
         server_socket.bind((ip, port))
-
         print(f"Server listening on {ip}:{port}")
+        drtp = DRTP(server_socket, ip, port, 'Ingenting')
+
+        with open(file_path, 'wb') as file:
+            while True:
+                data = drtp.receive()
+                if not data:
+                    break
+                file.write(data)
+        '''while True:
+            data = drtp.receive()
+            print(data)'''
 
     except OSError as e:
         print("Failed to bind. Error:", e)
         sys.exit()
 
-    handshake_complete = drtp.handle_handshake(server_socket)
-    if handshake_complete:
-        print("Handshake success")
-        packet, addr = server_socket.recvfrom(1472)
-        data = drtp.receive_data(server_socket, packet, addr[0], addr[1])
-        print(data)
 
-    else:
-        print("Handshake failed.")
-        sys.exit()
 
 def run_client(server_ip, server_port):
     file_path = '/Users/fahmimohammed/Screenshot 2023-04-24 at 22.58.35.png'
 
     try:
-        sender_socket = socket(AF_INET, SOCK_DGRAM)
+        sender_sock = socket(AF_INET,SOCK_DGRAM)
 
-        handshake_complete = drtp.initiate_handshake(sender_socket, server_ip, server_port)
-        if handshake_complete:
-            #send_packet(sender_socket, (server_ip, server_port), file_path)
-            print("Handshake complete")
-            with open(file_path, 'rb') as file:
+        drtp = DRTP(sender_sock, server_ip, server_port, 'Ingenting')
+        #send_packet(sender_socket, (server_ip, server_port), file_path)
+        print("Handshake complete")
+        with open(file_path, 'rb') as file:
+            data = file.read(1460)
+            while data:
+
+                drtp.send_and_wait(data)
                 data = file.read(1460)
-                drtp.stop_and_wait(sender_socket, server_ip, server_port, data)
-        else:
-            print("Handshake not complete")
-            sys.exit()
+
+        '''for i in range(15):
+            data = f"Her data nr. {i}"
+            drtp.send_and_wait(data.encode())'''
+
 
     except IOError:
         print("Error opening file")
